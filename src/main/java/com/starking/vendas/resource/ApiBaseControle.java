a10d57ad77ac3349da810399d7bcbaa6049494fb
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-15 20:05:11 -0300

    metodo de listar categorias

--- src/main/java/com/starking/vendas/resource/ApiBaseControle.java
+++ src/main/java/com/starking/vendas/resource/ApiBaseControle.java
@@ -3,10 +3,6 @@ package com.starking.vendas.resource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.starking.vendas.services.CategoriaService;
-
-import lombok.AllArgsConstructor;
-
 /**
  * @author pedroRhamon
  */
--- src/main/java/com/starking/vendas/resource/CategoriaResource.java
+++ src/main/java/com/starking/vendas/resource/CategoriaResource.java
@@ -1,7 +1,12 @@
 package com.starking.vendas.resource;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.starking.vendas.model.Categoria;
 import com.starking.vendas.services.CategoriaService;
 
 import lombok.AllArgsConstructor;
@@ -14,5 +19,10 @@ import lombok.AllArgsConstructor;
 public class CategoriaResource extends ApiBaseControle{
 	
 	private final CategoriaService categoriaService;
+	
+	@GetMapping
+	public ResponseEntity<List<Categoria>> listar() {
+		return ResponseEntity.ok(this.categoriaService.lista());
+	}
 
 }
