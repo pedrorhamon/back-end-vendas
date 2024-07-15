01d535855782ec39b8bbe7f6ce6b7af325860911
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-15 19:58:29 -0300

    criando o resource e o rest para mapeamento

---
+++ src/main/java/com/starking/vendas/resource/ApiBaseControle.java
@@ -0,0 +1,14 @@
+package com.starking.vendas.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author pedroRhamon
+ */
+
+@RestController
+@RequestMapping("/api/")
+public class ApiBaseControle {
+
+}
---
+++ src/main/java/com/starking/vendas/resource/CategoriaResource.java
@@ -0,0 +1,11 @@
+package com.starking.vendas.resource;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author pedroRhamon
+ */
+@RestController
+public class CategoriaResource extends ApiBaseControle{
+
+}
