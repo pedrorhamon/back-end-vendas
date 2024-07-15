2319e726943bbdc958345a42a498f91606d2a376
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-15 20:32:16 -0300

    criando metodo de criar e atualizar

--- src/main/java/com/starking/vendas/resource/CategoriaResource.java
+++ src/main/java/com/starking/vendas/resource/CategoriaResource.java
@@ -3,9 +3,9 @@ package com.starking.vendas.resource;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.starking.vendas.model.Categoria;
@@ -23,10 +23,15 @@ public class CategoriaResource extends ApiBaseControle{
 	private final CategoriaService categoriaService;
 	
 	
-	 @GetMapping
-	    public ResponseEntity<?> listar2() {
-	        List<Categoria> categorias = this.categoriaService.lista();
-	        return !categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.status(HttpStatus.NOT_FOUND).body("Nenhuma categoria encontrada.");
-	    }
+	@GetMapping
+	public ResponseEntity<?> listar() {
+		List<Categoria> categorias = this.categoriaService.lista();
+		return !categorias.isEmpty() ? ResponseEntity.ok(categorias) : ResponseEntity.status(HttpStatus.NOT_FOUND).body("Nenhuma categoria encontrada.");
+	}
+	
+//	@PostMapping
+//	public ResponseEntity<?> criar() {
+//		
+//	}
 
 }
--- src/main/java/com/starking/vendas/services/CategoriaService.java
+++ src/main/java/com/starking/vendas/services/CategoriaService.java
@@ -1,12 +1,16 @@
 package com.starking.vendas.services;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.starking.vendas.model.Categoria;
 import com.starking.vendas.repositories.CategoriaRepository;
 
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 
 /**
@@ -21,6 +25,23 @@ public class CategoriaService {
 	
 	public List<Categoria> lista() {
 		return this.repository.findAll();
+	}
+	
+	@Transactional
+	public void criar(Categoria categoria) {
+		Categoria categoriaNew = new Categoria();
+		categoriaNew.setName(categoria.getName());
+		categoriaNew.setCreatedAt(LocalDateTime.now());
+		
+		this.repository.save(categoriaNew);
+	}
+	
+	public void atualizar(Long id, Categoria categoriaAtualizada) {
+		repository.findById(id).map(categoriaExistente -> {
+			categoriaExistente.setName(categoriaAtualizada.getName());
+			categoriaExistente.setUpdatedAt(LocalDateTime.now());
+			return repository.save(categoriaExistente);
+		}).orElseThrow(() -> new EntityNotFoundException("Categoria não encontrada com o ID: " + id));
 	}
 
 }
