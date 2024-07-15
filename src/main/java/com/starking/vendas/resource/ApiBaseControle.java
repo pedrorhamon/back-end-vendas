84cba2f1722c7b44f95f228b1119ff90f44d32e7
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-15 20:00:10 -0300

    criando services

--- src/main/java/com/starking/vendas/resource/ApiBaseControle.java
+++ src/main/java/com/starking/vendas/resource/ApiBaseControle.java
@@ -3,12 +3,19 @@ package com.starking.vendas.resource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.starking.vendas.services.CategoriaService;
+
+import lombok.AllArgsConstructor;
+
 /**
  * @author pedroRhamon
  */
 
 @RestController
 @RequestMapping("/api/")
+@AllArgsConstructor
 public class ApiBaseControle {
+	
+	private final CategoriaService service;
 
 }
---
+++ src/main/java/com/starking/vendas/services/CategoriaService.java
@@ -0,0 +1,14 @@
+package com.starking.vendas.services;
+
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * @author pedroRhamon
+ */
+@Service
+@AllArgsConstructor
+public class CategoriaService {
+
+}
