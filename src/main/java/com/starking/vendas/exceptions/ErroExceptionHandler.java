c5cd911b3b38756e66ce1474b978113cbb9a5554
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-16 18:26:16 -0300

    tratamento de erros

--- src/main/java/com/starking/vendas/exceptions/ErroExceptionHandler.java
+++ src/main/java/com/starking/vendas/exceptions/ErroExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.starking.vendas.exceptions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
@@ -7,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -31,6 +35,25 @@ public class ErroExceptionHandler extends ResponseEntityExceptionHandler {
 		String mensagemDev = ex.getCause().toString();
 		return handleExceptionInternal(ex, new Erro(messageInvalida, mensagemDev), headers, HttpStatus.BAD_REQUEST, request);
 	}
+	
+	
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		
+		List<Erro> erros = criarListaDeErros();
+		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	private List<Erro> criarListaDeErros() {
+		List<Erro> erros = new ArrayList<>();
+		
+		return erros;
+	}
+
+
+
 
 	@Getter
 	@Setter
