590826dee4434f129e91185b5bc94435ed40d411
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-16 18:51:47 -0300

    inicio do controller

---
+++ src/main/java/com/starking/vendas/resource/PessoaController.java
@@ -0,0 +1,14 @@
+package com.starking.vendas.resource;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * @author pedroRhamon
+ */
+@RestController
+@AllArgsConstructor
+public class PessoaController extends ApiBaseControle{
+
+}
