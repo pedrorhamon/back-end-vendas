ebb459528de1c5ca768d6caa9597db3d73c71a48
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-16 19:03:24 -0300

    criando pessoa service

--- src/main/java/com/starking/vendas/services/PessoaService.java
+++ src/main/java/com/starking/vendas/services/PessoaService.java
@@ -1,13 +1,19 @@
 package com.starking.vendas.services;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import com.starking.vendas.model.Categoria;
+import com.starking.vendas.model.Pessoa;
+import com.starking.vendas.model.request.PessoaRequest;
+import com.starking.vendas.model.response.CategoriaResponse;
 import com.starking.vendas.model.response.PessoaResponse;
 import com.starking.vendas.repositories.PessoaRepository;
 
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 
 /**
@@ -23,6 +29,18 @@ public class PessoaService {
 	    return this.pessoaRepository.findAll().stream()
 	        .map(PessoaResponse::new) 
 	        .collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public PessoaResponse criar(PessoaRequest pessoaRequest) {
+		Pessoa pessoa = new Pessoa();
+		pessoa.setName(pessoaRequest.getName());
+		pessoa.setAtivo(pessoaRequest.getAtivo());
+		pessoa.setCreatedAt(LocalDateTime.now());
+
+		Pessoa pessoaSalva = this.pessoaRepository.save(pessoa);
+
+		return new PessoaResponse(pessoaSalva);
 	}
 
 }
