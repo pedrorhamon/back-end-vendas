65b045417f74ab176802d715f974ab42e16c2d69
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-17 19:52:03 -0300

    criando migration para meus domains

--- src/main/resources/db/migration/V01_01__create_table_categorias.sql
+++ src/main/resources/db/migration/V01_01__create_table_categorias.sql
@@ -1,7 +1,14 @@
-CREATE TABLE categoria 
+CREATE TABLE  categoria 
 (
   id BIGSERIAL PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
   createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+-- Insert categories
+INSERT INTO categoria (name) VALUES ('Categoria 1');
+INSERT INTO categoria (name) VALUES ('Categoria 2');
+INSERT INTO categoria (name) VALUES ('Categoria 3');
+INSERT INTO categoria (name) VALUES ('Categoria 4');
+INSERT INTO categoria (name) VALUES ('Categoria 5');
---
+++ src/main/resources/db/migration/V01_02__create_table_pessoa.sql
@@ -0,0 +1,17 @@
+CREATE TABLE pessoa (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    ativo BOOLEAN NOT NULL,
+    created_at TIMESTAMP NOT NULL
+);
+
+INSERT INTO pessoa (name, ativo, created_at) VALUES ('Pessoa 1', true, now());
+INSERT INTO pessoa (name, ativo, created_at) VALUES ('Pessoa 2', true, now());
+INSERT INTO pessoa (name, ativo, created_at) VALUES ('Pessoa 3', true, now());
+INSERT INTO pessoa (name, ativo, created_at) VALUES ('Pessoa 4', true, now());
+INSERT INTO pessoa (name, ativo, created_at) VALUES ('Pessoa 5', true, now());
+INSERT INTO pessoa (name, ativo, created_at) VALUES ('Pessoa 6', true, now());
+INSERT INTO pessoa (name, ativo, created_at) VALUES ('Pessoa 7', true, now());
+INSERT INTO pessoa (name, ativo, created_at) VALUES ('Pessoa 8', true, now());
+INSERT INTO pessoa (name, ativo, created_at) VALUES ('Pessoa 9', true, now());
+INSERT INTO pessoa (name, ativo, created_at) VALUES ('Pessoa 10', true, now());
