55144e71205134a33b50a50d0d47514ce21d4797
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-18 19:38:48 -0300

    criando endpoint de listar todos lancamentos

--- src/main/java/com/starking/vendas/resource/LancamentoResource.java
+++ src/main/java/com/starking/vendas/resource/LancamentoResource.java
@@ -1,7 +1,14 @@
 package com.starking.vendas.resource;
 
+import java.util.List;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.starking.vendas.model.response.LancamentoResponse;
 import com.starking.vendas.services.LancamentoService;
 
 import lombok.AllArgsConstructor;
@@ -14,5 +21,13 @@ import lombok.AllArgsConstructor;
 public class LancamentoResource extends ApiLancamentoBaseControle{
 	
 	private final LancamentoService lancamentoService;
+	
+private final ApplicationEventPublisher publisher;
+	
+	@GetMapping
+	public ResponseEntity<?> listar() {
+		List<LancamentoResponse> lancamentos = this.lancamentoService.listarTodos();
+		return !lancamentos.isEmpty() ? ResponseEntity.ok(lancamentos) : ResponseEntity.status(HttpStatus.NOT_FOUND).body("Nenhum Lançamento foi encontrado.");
+	}
 
 }
