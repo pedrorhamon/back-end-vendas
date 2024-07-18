92267a3c30e5dfd239b267bfe5f512f363f7852b
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-18 19:17:56 -0300

    lancamentoRequest

---
+++ src/main/java/com/starking/vendas/model/request/LancamentoRequest.java
@@ -0,0 +1,63 @@
+package com.starking.vendas.model.request;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.starking.vendas.model.Lancamento;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author pedroRhamon
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LancamentoRequest {
+	
+	private Long id;
+	
+	@NotNull
+	@NotBlank
+	private String descricao;
+	
+	@DateTimeFormat(pattern = "dd/MM/yyyy HH:mm")
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+	private LocalDateTime dataVencimento;
+	
+	@DateTimeFormat(pattern = "dd/MM/yyyy HH:mm")
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+	private LocalDateTime dataPagamento;
+	
+	private BigDecimal valor;
+	
+	private String observacao;
+	
+	private String tipo;
+	
+	private Long categoriaId;
+	
+	private Long pessoaId;
+	
+	public LancamentoRequest(Lancamento entity) {
+		this.id = entity.getId();
+		this.descricao = entity.getDescricao();
+		this.dataVencimento = entity.getDataVencimento();
+		this.dataPagamento = entity.getDataPagamento();
+		this.valor = entity.getValor();
+		this.observacao = entity.getObservacao();
+		this.tipo = entity.getTipoLancamento().name();
+		this.categoriaId = entity.getCategoria().getId();
+		this.pessoaId = entity.getPessoa().getId();
+	}
+
+}
