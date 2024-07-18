85594e4f9f2239641a9858da194525965d244002
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-18 19:23:11 -0300

    listar todos os lancamentos

--- src/main/java/com/starking/vendas/services/LancamentoService.java
+++ src/main/java/com/starking/vendas/services/LancamentoService.java
@@ -1,7 +1,11 @@
 package com.starking.vendas.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
+import com.starking.vendas.model.response.LancamentoResponse;
 import com.starking.vendas.repositories.LancamentoRepository;
 
 import lombok.AllArgsConstructor;
@@ -14,5 +18,11 @@ import lombok.AllArgsConstructor;
 public class LancamentoService {
 	
 	private final LancamentoRepository lancamentoRepository;
+	
+	public List<LancamentoResponse> listarTodos() {
+	    return this.lancamentoRepository.findAll().stream()
+	        .map(LancamentoResponse::new) 
+	        .collect(Collectors.toList());
+	}
 
 }
