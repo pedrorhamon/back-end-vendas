85ef96bcaec59c35301b3a123ac5f287ee069d55
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-18 19:09:15 -0300

    colocando outros atributos

--- src/main/java/com/starking/vendas/model/response/LancamentoResponse.java
+++ src/main/java/com/starking/vendas/model/response/LancamentoResponse.java
@@ -1,5 +1,10 @@
 package com.starking.vendas.model.response;
 
+import java.time.LocalDateTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.starking.vendas.model.Lancamento;
 
 import jakarta.validation.constraints.NotBlank;
@@ -20,6 +25,14 @@ public class LancamentoResponse {
 	@NotNull
 	@NotBlank
 	private String descricao;
+	
+	@DateTimeFormat(pattern = "dd/MM/yyyy HH:mm")
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+	private LocalDateTime dataVencimento;
+	
+	@DateTimeFormat(pattern = "dd/MM/yyyy HH:mm")
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+	private LocalDateTime dataPagamento;
 	
 	public LancamentoResponse(Lancamento entity) {
 		this.id = entity.getId();
