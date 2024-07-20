83688173f6d01d808c24524e9d452afb157a93ac
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-20 19:25:10 -0300

    criando service de deletar

--- src/main/java/com/starking/vendas/services/LancamentoService.java
+++ src/main/java/com/starking/vendas/services/LancamentoService.java
@@ -121,5 +121,13 @@ public class LancamentoService {
 	        return new LancamentoResponse(lancamentoAtualizado);
 	    }).orElseThrow(() -> new EntityNotFoundException("Lancamento not found with ID: " + id));
 	}
+	
+	 @Transactional
+	    public void deletarLancamento(Long id) {
+	        Lancamento lancamento = lancamentoRepository.findById(id)
+	            .orElseThrow(() -> new EntityNotFoundException("Lançamento não encontrado com o ID: " + id));
+	        lancamentoRepository.delete(lancamento);
+	    }
+
 
 }
