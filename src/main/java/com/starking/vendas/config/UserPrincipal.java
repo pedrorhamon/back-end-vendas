f5e7a455da0989041295520d101d206816dd3f8c
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-20 20:02:11 -0300

    Criando entidade de Permissoes

--- src/main/java/com/starking/vendas/config/UserPrincipal.java
+++ src/main/java/com/starking/vendas/config/UserPrincipal.java
@@ -1,8 +1,33 @@
 package com.starking.vendas.config;
 
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import lombok.Getter;
+
 /**
  * @author pedroRhamon
  */
+@Getter
 public class UserPrincipal {
+    private String username;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
 
+    private UserPrincipal(User user){
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+
+        this.authorities = user.getRoles().stream().map(role -> {
+            return new SimpleGrantedAuthority("ROLE_".concat(role.getName()));
+        }).collect(Collectors.toList());
+    }
+
+    public static UserPrincipal create(User user){
+        return new UserPrincipal(user);
+    }
 }
--- src/main/java/com/starking/vendas/model/Usuario.java
+++ src/main/java/com/starking/vendas/model/Usuario.java
@@ -1,8 +1,36 @@
 package com.starking.vendas.model;
 
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
 /**
  * @author pedroRhamon
  */
-public class Usuario {
+
+@Entity
+@Table(name = "usuario")
+@Data
+public class Usuario implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+
+	private String email;
+	
+	private String senha;
+
+//	@ManyToMany
+//	private List<Role> roles;
 
 }
