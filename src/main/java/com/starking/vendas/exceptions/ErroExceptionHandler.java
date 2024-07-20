dfcd6648bc5a842bf7f01bab6cedfe3981feb8bb
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-19 23:20:11 -0300

    adicionando novas message.properties

--- src/main/java/com/starking/vendas/exceptions/ErroExceptionHandler.java
+++ src/main/java/com/starking/vendas/exceptions/ErroExceptionHandler.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -15,6 +16,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -38,6 +40,14 @@ public class ErroExceptionHandler extends ResponseEntityExceptionHandler {
 		String mensagemDev = ex.getCause().toString();
 		List<Erro> erros = Arrays.asList(new Erro(messageInvalida, mensagemDev));
 		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	@ExceptionHandler({ DataIntegrityViolationException.class})
+	public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request) {
+		String messageInvalida = messageSource.getMessage("mensagem-invalida", null, LocaleContextHolder.getLocale());
+		String mensagemDev = ex.getCause().toString();
+		List<Erro> erros = Arrays.asList(new Erro(messageInvalida, mensagemDev));
+		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
 	
 	
