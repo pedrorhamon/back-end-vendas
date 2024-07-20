bb924bc231685ec0374e18e8630ea30790098541
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-20 00:05:04 -0300

    removendo imports

--- src/main/java/com/starking/vendas/services/LancamentoService.java
+++ src/main/java/com/starking/vendas/services/LancamentoService.java
@@ -3,7 +3,6 @@ package com.starking.vendas.services;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
