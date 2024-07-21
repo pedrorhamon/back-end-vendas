da8bdc7d1db6dfe4336148e78f2c227067a28322
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-20 22:36:45 -0300

    extends para base do endpoint

---
+++ src/main/java/com/starking/vendas/resource/ApiPermissaoBaseControle.java
@@ -0,0 +1,12 @@
+package com.starking.vendas.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author pedroRhamon
+ */
+
+@RestController
+@RequestMapping("/api/permissoes")
+public class ApiPermissaoBaseControle {}
