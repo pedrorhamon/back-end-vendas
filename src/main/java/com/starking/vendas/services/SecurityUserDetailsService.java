f00982a058f0323825f5c9efb7f39f928d7e34c8
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-20 21:27:35 -0300

    adicionando as roles

--- src/main/java/com/starking/vendas/services/SecurityUserDetailsService.java
+++ src/main/java/com/starking/vendas/services/SecurityUserDetailsService.java
@@ -1,7 +1,14 @@
 package com.starking.vendas.services;
 
-import org.springframework.security.core.userdetails.*;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.starking.vendas.model.Usuario;
@@ -24,9 +31,15 @@ public class SecurityUserDetailsService implements UserDetailsService{
 				.findByEmail(email)
 				.orElseThrow(() -> new UsernameNotFoundException("Email não cadastrado"));
 		
+		 List<GrantedAuthority> authorities = usuarioEncontrado.getPermissoes()
+	                .stream()
+	                .map(permissao -> new SimpleGrantedAuthority(permissao.getName()))
+	                .collect(Collectors.toList());
+		
 		return User.builder()
 				.username(usuarioEncontrado.getEmail())
 				.password(usuarioEncontrado.getSenha())
+				.authorities(authorities)
 				.build();
 	}
 }
