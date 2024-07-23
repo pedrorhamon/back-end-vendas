53b7a6f9637189f6ca8ed6aca692fbc35c315893
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-23 18:25:50 -0300

    criando conexao com RestTemplate

--- src/main/java/com/starking/vendas/infra/ViaCepService.java
+++ src/main/java/com/starking/vendas/infra/ViaCepService.java
@@ -1,6 +1,9 @@
 package com.starking.vendas.infra;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.starking.vendas.model.embedded.Endereco;
 
 /**
  * @author pedroRhamon
@@ -8,4 +11,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class ViaCepService {
 
+	private static final String VIACEP_URL = "https://viacep.com.br/ws/%s/json/";
+
+	public Endereco buscarEnderecoPorCep(String cep) {
+		RestTemplate restTemplate = new RestTemplate();
+		Endereco endereco = restTemplate.getForObject(String.format(VIACEP_URL, cep), Endereco.class);
+		return endereco;
+	}
 }
