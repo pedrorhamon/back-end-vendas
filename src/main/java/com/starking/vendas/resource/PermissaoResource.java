e28ff9ae83a6f4c31802d2ab424b372cf2305a45
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-23 18:47:35 -0300

    removendo imports

--- src/main/java/com/starking/vendas/resource/PermissaoResource.java
+++ src/main/java/com/starking/vendas/resource/PermissaoResource.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
