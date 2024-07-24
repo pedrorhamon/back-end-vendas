45cd7ba5b63293a5c1a4d3cd6b4036b59d54cbc4
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-24 18:03:11 -0300

    class Service de Email criada

--- src/main/java/com/starking/vendas/services/EmailService.java
+++ src/main/java/com/starking/vendas/services/EmailService.java
@@ -1,7 +1,11 @@
 package com.starking.vendas.services;
 
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.AllArgsConstructor;
 
 /**
@@ -11,5 +15,18 @@ import lombok.AllArgsConstructor;
 @Service
 @AllArgsConstructor
 public class EmailService {
+	
+	private final JavaMailSender mailSender;
+	
+	 public void sendEmail(String to, String subject, String text) throws MessagingException {
+	        MimeMessage message = mailSender.createMimeMessage();
+	        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+	        helper.setTo(to);
+	        helper.setSubject(subject);
+	        helper.setText(text, true); 
+
+	        mailSender.send(message);
+	    }
 
 }
