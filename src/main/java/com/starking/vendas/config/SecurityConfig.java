17925d2cfe7ca3ddb6a5ec5063548c7549a47f65
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-24 18:00:51 -0300

    adicionando dependency de envio de email

--- src/main/java/com/starking/vendas/config/SecurityConfig.java
+++ src/main/java/com/starking/vendas/config/SecurityConfig.java
@@ -1,27 +1,17 @@
 package com.starking.vendas.config;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
-import com.starking.vendas.model.Permissao;
 import com.starking.vendas.services.JwtService;
 import com.starking.vendas.services.SecurityUserDetailsService;
 
