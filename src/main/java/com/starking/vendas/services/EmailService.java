f6b297c3350f469e8032a20e95cc61d423e5a8a3
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-24 18:02:14 -0300

    class de service e configuracao de email

---
+++ src/main/java/com/starking/vendas/services/EmailService.java
@@ -0,0 +1,15 @@
+package com.starking.vendas.services;
+
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * @author pedroRhamon
+ */
+
+@Service
+@AllArgsConstructor
+public class EmailService {
+
+}
