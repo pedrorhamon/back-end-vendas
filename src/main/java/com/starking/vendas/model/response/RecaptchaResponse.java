c54dad306d750c9e052e0cae709f9e68ba028a83
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-25 19:51:44 -0300

    adicionando atributos

--- src/main/java/com/starking/vendas/model/response/RecaptchaResponse.java
+++ src/main/java/com/starking/vendas/model/response/RecaptchaResponse.java
@@ -1,8 +1,24 @@
 package com.starking.vendas.model.response;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * @author pedroRhamon
  */
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class RecaptchaResponse {
+	
+	private boolean success;
+    @JsonProperty("error-codes")
+    private List<String> errorCodes;
 
 }
