c94ba9dd40a98de57d25b1343d8f893410e7d48c
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-07-26 18:00:36 -0300

    tratamento de cors

--- src/main/java/com/starking/vendas/config/SecurityConfig.java
+++ src/main/java/com/starking/vendas/config/SecurityConfig.java
@@ -1,7 +1,12 @@
 package com.starking.vendas.config;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +16,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.starking.vendas.services.JwtService;
 import com.starking.vendas.services.SecurityUserDetailsService;
@@ -99,28 +107,28 @@ public class SecurityConfig {
 //        return registrationBean;
 //    }
     
-//    @Bean
-//	public FilterRegistrationBean<CorsFilter> corsFilter(){
-//		
-//		List<String> all = Arrays.asList("http://localhost:3000");
-//		
-//		CorsConfiguration config = new CorsConfiguration();
-//		config.setAllowedMethods(all);
-//		config.setAllowedOrigins(all);
-//		config.setAllowedHeaders(all);
-//		config.setAllowCredentials(true);
-//		
-//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//		source.registerCorsConfiguration("/**", config);
-//		
-//		CorsFilter corFilter = new CorsFilter(source);
-//		
-//		FilterRegistrationBean<CorsFilter> filter = 
-//				new FilterRegistrationBean<CorsFilter>(corFilter);
-//		filter.setOrder(Ordered.HIGHEST_PRECEDENCE);
-//		
-//		return filter;
-//	}
+    @Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter(){
+		
+		List<String> all = Arrays.asList("http://localhost:4200");
+		
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedMethods(all);
+		config.setAllowedOrigins(all);
+		config.setAllowedHeaders(all);
+		config.setAllowCredentials(true);
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		
+		CorsFilter corFilter = new CorsFilter(source);
+		
+		FilterRegistrationBean<CorsFilter> filter = 
+				new FilterRegistrationBean<CorsFilter>(corFilter);
+		filter.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		
+		return filter;
+	}
 
 
 }
