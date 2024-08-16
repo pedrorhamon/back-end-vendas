0191cbbabad5ae744376623b5547c25c22911b40
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-08-16 15:52:54 -0300

    No momento deixar comentando

--- src/main/java/com/starking/vendas/config/WebConfiguration.java
+++ src/main/java/com/starking/vendas/config/WebConfiguration.java
@@ -1,20 +1,21 @@
-package com.starking.vendas.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@EnableWebMvc
-@Configuration
-public class WebConfiguration implements WebMvcConfigurer {
-
-	@Override
-	public void addCorsMappings( CorsRegistry registry ) {
-		registry.addMapping("/**")
-		 .allowedOrigins("http://localhost")
-		 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-		 .allowedHeaders("*")
-         .allowCredentials(true);
-	}
-}
+//package com.starking.vendas.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@EnableWebMvc
+//@Configuration
+//public class WebConfiguration implements WebMvcConfigurer {
+//
+//	@Override
+//	public void addCorsMappings( CorsRegistry registry ) {
+//		registry.addMapping("/**")
+//		 .allowedOrigins("http://localhost")
+//		 .allowedOrigins("http://localhost:4200")
+//		 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//		 .allowedHeaders("*")
+//         .allowCredentials(true);
+//	}
+//}
