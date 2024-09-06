b20075fe8d2248a835cfa8005707ad28aced5a0f
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-09-06 19:48:32 -0300

    metodo de upload de image

--- src/main/java/com/starking/vendas/services/CategoriaService.java
+++ src/main/java/com/starking/vendas/services/CategoriaService.java
@@ -1,10 +1,17 @@
 package com.starking.vendas.services;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.starking.vendas.model.Categoria;
 import com.starking.vendas.model.request.CategoriaRequest;
@@ -45,6 +52,17 @@ public class CategoriaService {
 		Categoria categoriaSalva = this.repository.save(categoria);
 
 		return new CategoriaResponse(categoriaSalva);
+	}
+	
+	private String uploadImage(MultipartFile imageFile) throws IOException {
+		String directory = "/path/to/images/";
+	    String fileName = UUID.randomUUID().toString() + "_" + imageFile.getOriginalFilename();
+	    Path filePath = Paths.get(directory + fileName);
+
+	    Files.copy(imageFile.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+
+	    // Retorne a URL da imagem (pode ser uma URL local ou externa)
+	    return "http://localhost:8080/images/" + fileName;
 	}
 	
 	public CategoriaResponse atualizar(Long id, CategoriaRequest categoriaRequest) {
