e875526917586fd038b1e05030960f6ccfc869d5
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-10-05 14:08:18 -0300

    criando camada de repository de test para Permissao

---
+++ src/test/java/com/starking/vendas/repositories/PermissaoRepositoryTest.java
@@ -0,0 +1,13 @@
+package com.starking.vendas.repositories;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class PermissaoRepositoryTest {
+
+    @Autowired
+    private PermissaoRepository permissaoRepository;
+
+
+}
