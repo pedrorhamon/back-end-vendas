c3f775d1a65251a95ce62d29163e89d443c6535f
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-10-05 11:55:10 -0300

    testFindByNome

--- src/test/java/com/starking/vendas/repositories/CategorioRepositoryTest.java
+++ src/test/java/com/starking/vendas/repositories/CategorioRepositoryTest.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -27,6 +29,21 @@ public class CategorioRepositoryTest {
         assertEquals("Categoria 1", categoriaEncontrada.get().getName());
     }
 
+    @Test
+    public void testFindByNome() {
+        Categoria categoria = new Categoria();
+        categoria.setName("Categoria 1");
+        categoriaRepository.save(categoria);
 
+        Categoria categoria2 = new Categoria();
+        categoria2.setName("Categoria 2");
+        categoriaRepository.save(categoria2);
 
+        List<String> nomesCategorias = Arrays.asList("Categoria 1", "Categoria 2");
+        List<Categoria> categoriasEncontradas = categoriaRepository.findByNameIn(nomesCategorias);
+
+        assertEquals(2, categoriasEncontradas.size());
+        assertTrue(categoriasEncontradas.stream().anyMatch(c -> c.getName().equals("Categoria 1")));
+        assertTrue(categoriasEncontradas.stream().anyMatch(c -> c.getName().equals("Categoria 2")));
+    }
 }
