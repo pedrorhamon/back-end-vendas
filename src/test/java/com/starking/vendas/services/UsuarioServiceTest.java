7f4ca58161f0a4da2d3db9c357219c2a3aac4bcf
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-10-05 16:43:01 -0300

    iniciando o setup

--- src/test/java/com/starking/vendas/services/UsuarioServiceTest.java
+++ src/test/java/com/starking/vendas/services/UsuarioServiceTest.java
@@ -1,4 +1,22 @@
 package com.starking.vendas.services;
 
+import com.starking.vendas.repositories.UsuarioRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
 public class UsuarioServiceTest {
+
+    @Mock
+    private UsuarioRepository repository;
+
+    @InjectMocks
+    private UsuarioService usuarioService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
 }
