adcac4366ced32de96feb9c8577b227a6c756dd1
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-10-05 16:44:48 -0300

    adicionando o restante

--- src/test/java/com/starking/vendas/services/UsuarioServiceTest.java
+++ src/test/java/com/starking/vendas/services/UsuarioServiceTest.java
@@ -1,15 +1,26 @@
 package com.starking.vendas.services;
 
+import com.starking.vendas.config.JwtTokenFilter;
 import com.starking.vendas.repositories.UsuarioRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class UsuarioServiceTest {
 
     @Mock
     private UsuarioRepository repository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private EmailService emailService;
+
+    @Mock
+    private JwtTokenFilter jwtTokenFilter;
 
     @InjectMocks
     private UsuarioService usuarioService;
