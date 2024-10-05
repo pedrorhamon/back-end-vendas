e22dca6a3ce4092d4ffeb1024702ae8c58998367
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-10-05 12:47:55 -0300

    setUp para Permissao

--- src/test/java/com/starking/vendas/services/PermissaoServiceTest.java
+++ src/test/java/com/starking/vendas/services/PermissaoServiceTest.java
@@ -1,4 +1,21 @@
 package com.starking.vendas.services;
 
+import com.starking.vendas.repositories.PermissaoRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
 public class PermissaoServiceTest {
+
+    @Mock
+    private PermissaoRepository repository;
+
+    @InjectMocks
+    private PermissaoService service;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
 }
