f571114d05f774b30b40fb62de34a51e7c2c23d6
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-10-05 09:38:03 -0300

    adicionando outros tests

--- src/test/java/com/starking/vendas/services/CategorioServiceTest.java
+++ src/test/java/com/starking/vendas/services/CategorioServiceTest.java
@@ -1,11 +1,9 @@
 package com.starking.vendas.services;
 
-import static org.mockito.Mockito.*;
-import static org.junit.jupiter.api.Assertions.*;
-
 import com.starking.vendas.model.Categoria;
 import com.starking.vendas.model.response.CategoriaResponse;
 import com.starking.vendas.repositories.CategoriaRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -17,6 +15,10 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.Arrays;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 public class CategorioServiceTest {
 
@@ -46,5 +48,35 @@ public class CategorioServiceTest {
         assertNotNull(result);
         assertEquals(1, result.getTotalElements());
         assertEquals("Categoria 1", result.getContent().get(0).getName());
+    }
+
+    @Test
+    public void testFindById() {
+        Long id = 1L;
+        Categoria categoria = new Categoria();
+        categoria.setId(id);
+        categoria.setName("Categoria 1");
+
+        when(repository.findById(id)).thenReturn(Optional.of(categoria));
+
+        CategoriaResponse result = service.findById(id);
+
+        assertNotNull(result);
+        assertEquals("Categoria 1", result.getName());
+    }
+
+    @Test
+    public void testFindByIdNotFound() {
+        Long id = 2L;
+        when(repository.findById(id)).thenReturn(Optional.empty());
+
+        Exception exception = assertThrows(EntityNotFoundException.class, () -> {
+            service.findById(id);
+        });
+
+        String expectedMessage = "Categoria not found";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
     }
 }
