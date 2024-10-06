2fd199412bbf42372731a1310729d133ef41d7a8
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-10-06 16:43:34 -0300

    case de cobertura de repository de testFindById()

--- src/test/java/com/starking/vendas/repositories/PessoaRepositoryTest.java
+++ src/test/java/com/starking/vendas/repositories/PessoaRepositoryTest.java
@@ -1,7 +1,35 @@
 package com.starking.vendas.repositories;
 
+import com.starking.vendas.model.Pessoa;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
 public class PessoaRepositoryTest {
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    @Test
+    public void testFindById() {
+        Pessoa pessoa = new Pessoa();
+        pessoa.setName("Pessoa 1");
+        pessoa.setAtivo(true);
+        pessoaRepository.save(pessoa);
+
+        Pageable pageable = PageRequest.of(0, 10);
+
+        Page<Pessoa> pessoaPage = pessoaRepository.findById(pessoa.getId(), pageable);
+
+        assertNotNull(pessoaPage);
+        assertEquals(1, pessoaPage.getTotalElements());
+        assertEquals("Pessoa 1", pessoaPage.getContent().get(0).getName());
+    }
 }
