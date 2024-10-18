03cc32be070371242c8556bbf05e2023212ddaf6
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-10-18 20:47:18 -0300

    comentando depois eu resolvo

--- src/main/java/com/starking/vendas/config/PaymentConfig.java
+++ src/main/java/com/starking/vendas/config/PaymentConfig.java
@@ -1,13 +1,13 @@
-package com.starking.vendas.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class PaymentConfig {
-
-    @Bean
-    public com.starking.payment.PaymentService paymentService() {
-        return com.starking.payment.StripePaymentService();
-    }
-}
+//package com.starking.vendas.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class PaymentConfig {
+//
+//    @Bean
+//    public com.starking.payment.PaymentService paymentService() {
+//        return com.starking.payment.StripePaymentService();
+//    }
+//}
