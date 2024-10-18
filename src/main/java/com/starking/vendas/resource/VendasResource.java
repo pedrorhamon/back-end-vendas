9dfc83c9f1527f584612d2af5d764dded3fa9d0b
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-10-18 20:11:44 -0300

    ajustes em algumas configurações

--- src/main/java/com/starking/vendas/resource/VendasResource.java
+++ src/main/java/com/starking/vendas/resource/VendasResource.java
@@ -1,11 +1,14 @@
-package com.starking.vendas.resource;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/vendas")
-public class VendasResource {
-
-    private final VendasService vendasService;
-}
+//package com.starking.vendas.resource;
+//
+//import com.starking.vendas.services.VendasService;
+//import lombok.AllArgsConstructor;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/api/vendas")
+//@AllArgsConstructor
+//public class VendasResource {
+//
+//    private final VendasService vendasService;
+//}
---
+++ src/main/java/com/starking/vendas/services/VendasService.java
@@ -0,0 +1,12 @@
+//package com.starking.vendas.services;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class VendasService {
+//
+//    @Autowired
+//    private com.starking.payment.PaymentService paymentService;
+//
+//}
