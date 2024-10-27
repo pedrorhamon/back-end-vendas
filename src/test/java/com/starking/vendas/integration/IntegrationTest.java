f060fea773936830aa97ef0df725b4e2cbf92408
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-10-27 16:34:23 -0300

    Test de integracion

--- src/test/java/com/starking/vendas/integration/IntegrationTest.java
+++ src/test/java/com/starking/vendas/integration/IntegrationTest.java
@@ -1,4 +1,25 @@
 package com.starking.vendas.integration;
 
+import lombok.AllArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AllArgsConstructor
 public class IntegrationTest {
+
+    private final TestRestTemplate restTemplate;
+
+    @Test
+    public void testEndpoint() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(200);
+        assertThat(response.getBody()).contains("expectedResponse");
+    }
+
 }
+
