741c67e8f5d8d3999007eca45c73ccea1f42504a
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-11-09 17:05:29 -0300

    criando setup na aplicação para SeleniumUITest

--- src/test/java/com/starking/vendas/selenium/SeleniumUITest.java
+++ src/test/java/com/starking/vendas/selenium/SeleniumUITest.java
@@ -1,4 +1,20 @@
 package com.starking.vendas.selenium;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class SeleniumUITest {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+
+        driver.get("http://localhost:4200");
+    }
 }
