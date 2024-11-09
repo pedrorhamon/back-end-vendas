b69748f3c8fa8c37a5136e0438ced3a47819413b
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-11-09 17:46:29 -0300

    testHomePage

--- src/test/java/com/starking/vendas/selenium/SeleniumUITest.java
+++ src/test/java/com/starking/vendas/selenium/SeleniumUITest.java
@@ -1,9 +1,14 @@
 package com.starking.vendas.selenium;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SeleniumUITest {
 
@@ -16,5 +21,14 @@ public class SeleniumUITest {
         driver = new ChromeDriver(options);
 
         driver.get("http://localhost:4200");
+    }
+
+    @Test
+    public void testHomePage() {
+        WebElement button = driver.findElement(By.id("buttonId"));
+        button.click();
+
+        WebElement result = driver.findElement(By.id("resultTextId"));
+        assertThat(result.getText()).isEqualTo("Texto Esperado");
     }
 }
