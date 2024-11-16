6099be93a81d5bb2b31a350590cb148e2475e817
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-11-16 15:23:59 -0300

    Repository

---
+++ src/main/java/com/starking/vendas/repositories/SubPermissaoRepository.java
@@ -0,0 +1,18 @@
+package com.starking.vendas.repositories;
+
+import com.starking.vendas.model.Permissao;
+import com.starking.vendas.model.SubPermissao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * @author pedroRhamon
+ */
+public interface SubPermissaoRepository extends JpaRepository<SubPermissao, Long>{
+
+	List<SubPermissao> findByPermissaoPrincipal(Permissao permissaoPrincipal);
+
+	Long findById(long id);
+
+}
