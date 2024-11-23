7826ca48bea10ba6dfc28eaa47b470ae1e58f12b
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-11-23 17:53:48 -0300

    listas todas com paginação

--- src/main/java/com/starking/vendas/resource/SubPermissaoResource.java
+++ src/main/java/com/starking/vendas/resource/SubPermissaoResource.java
@@ -1,10 +1,35 @@
 package com.starking.vendas.resource;
 
+import com.starking.vendas.model.response.SubPermissaoResponse;
 import com.starking.vendas.resource.apis_base.ApiPermissaoBaseControle;
+import com.starking.vendas.services.SubPermissaoService;
 import lombok.AllArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
 public class SubPermissaoResource extends ApiPermissaoBaseControle {
+
+    private final SubPermissaoService subPermissaoService;
+
+    private final ApplicationEventPublisher publisher;
+
+    @GetMapping
+    public ResponseEntity<Page<SubPermissaoResponse>> listar(
+            @RequestParam(required = false) Long id,
+            @RequestParam(required = false) String descricao,
+            @PageableDefault(size = 10) Pageable pageable) {
+
+        Page<SubPermissaoResponse> permissoes = subPermissaoService.listarTodas(pageable);
+        return !permissoes.isEmpty() ? ResponseEntity.ok(permissoes)
+                : ResponseEntity.status(HttpStatus.NOT_FOUND).body(Page.empty());
+    }
 }
