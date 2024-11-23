6d5c9af5268afee10690d0cf9235569af362a911
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-11-22 22:19:08 -0300

    SubPermissaoResource

---
+++ src/main/java/com/starking/vendas/resource/SubPermissaoResource.java
@@ -0,0 +1,10 @@
+package com.starking.vendas.resource;
+
+import com.starking.vendas.resource.apis_base.ApiPermissaoBaseControle;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class SubPermissaoResource extends ApiPermissaoBaseControle {
+}
