afd0b136893056d051dc97c34886e52d956b2a70
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-11-23 17:57:58 -0300

    endpoint criado para SubPermissaoResource

--- src/main/java/com/starking/vendas/resource/SubPermissaoResource.java
+++ src/main/java/com/starking/vendas/resource/SubPermissaoResource.java
@@ -1,8 +1,14 @@
 package com.starking.vendas.resource;
 
+import com.starking.vendas.event.RecursoCriadoEvent;
+import com.starking.vendas.model.request.SubPermissaoRequest;
+import com.starking.vendas.model.response.PermissaoResponse;
 import com.starking.vendas.model.response.SubPermissaoResponse;
 import com.starking.vendas.resource.apis_base.ApiPermissaoBaseControle;
 import com.starking.vendas.services.SubPermissaoService;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import jakarta.validation.ValidationException;
 import lombok.AllArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Page;
@@ -10,9 +16,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -28,8 +32,22 @@ public class SubPermissaoResource extends ApiPermissaoBaseControle {
             @RequestParam(required = false) String descricao,
             @PageableDefault(size = 10) Pageable pageable) {
 
-        Page<SubPermissaoResponse> permissoes = subPermissaoService.listarTodas(pageable);
-        return !permissoes.isEmpty() ? ResponseEntity.ok(permissoes)
+        Page<SubPermissaoResponse> subPermissoes = subPermissaoService.listarTodas(pageable);
+        return !subPermissoes.isEmpty() ? ResponseEntity.ok(subPermissoes)
                 : ResponseEntity.status(HttpStatus.NOT_FOUND).body(Page.empty());
+    }
+
+    @PostMapping
+    public ResponseEntity<?> criar(@Valid @RequestBody SubPermissaoRequest subPermissaoRequest, HttpServletResponse response) {
+        try {
+            SubPermissaoResponse subPermissoesNew = this.subPermissaoService.salvar(subPermissaoRequest);
+
+            publisher.publishEvent(new RecursoCriadoEvent(this, response, subPermissoesNew.getId()));
+            return ResponseEntity.status(HttpStatus.CREATED).body(subPermissoesNew);
+        } catch (ValidationException e) {
+            return ResponseEntity.badRequest().body("Erro de validação: " + e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ocorreu um erro ao criar a SubPermissão.");
+        }
     }
 }
