502f8b7a06f8bb197bad400538cfde65450f98f7
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-11-30 14:33:17 -0300

    Resolvendo problema com email

---
+++ src/main/java/com/starking/vendas/config/EmailConfiguration.java
@@ -0,0 +1,32 @@
+package com.starking.vendas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfiguration {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost("smtp.gmail.com"); // Altere para o host do seu provedor
+        mailSender.setPort(587); // Porta para STARTTLS
+
+        mailSender.setUsername("463938c54f3b25"); // Substitua pelo seu email
+        mailSender.setPassword("8ab42b32d0e4ef");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
--- src/main/java/com/starking/vendas/services/EmailService.java
+++ src/main/java/com/starking/vendas/services/EmailService.java
@@ -1,5 +1,6 @@
 package com.starking.vendas.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
