23a40d32694ca16e97f566fa5fe5357b6699bd86
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-12-22 17:52:04 -0300

    criando query para raio

--- src/main/java/com/starking/vendas/repositories/PessoaRepository.java
+++ src/main/java/com/starking/vendas/repositories/PessoaRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.starking.vendas.model.Pessoa;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  * @author pedroRhamon
@@ -18,5 +20,9 @@ public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
 	Page<Pessoa> findByName(String name, Pageable pageable);
 
 	List<Pessoa> findByNameIn(List<String> pessoaNomes);
+
+	@Query("SELECT p FROM Pessoa p WHERE ST_DWithin(p.coordenadas, ST_SetSRID(ST_MakePoint(:longitude, :latitude), 4326), :raio) = true")
+	List<Pessoa> findByProximidade(@Param("longitude") Double longitude, @Param("latitude") Double latitude, @Param("raio") Double raio);
+
 
 }
