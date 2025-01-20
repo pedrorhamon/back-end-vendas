09eca628df4b84f16ae67486110e9450736e8eb9
Pedro Rhamon <pedrorhamon16@gmail.com>
2025-01-20 19:26:55 -0300

    Mensagens para Lançamento

--- src/main/java/com/starking/vendas/utils/MessagesUtils.java
+++ src/main/java/com/starking/vendas/utils/MessagesUtils.java
@@ -4,4 +4,9 @@ public class MessagesUtils {
 
     private static final String NAO_POSSIVEL_CADASTRAR = "Não é possivel cadastrar";
     private static final String USUARIO_INATIVO = "Usuário inativo";
+
+    // Mensagens para Lançamento
+    private static final String LANCAMENTO_NAO_ENCONTRADO = "Lançamento não encontrado";
+    private static final String LANCAMENTO_JA_EXISTE = "Lançamento já existe";
+    private static final String ERRO_AO_SALVAR_LANCAMENTO = "Erro ao salvar o lançamento";
 }
