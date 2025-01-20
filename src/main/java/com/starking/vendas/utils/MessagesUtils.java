222580040989ed9a25ff15efafd5c3c5c2ef5205
Pedro Rhamon <pedrorhamon16@gmail.com>
2025-01-20 19:27:24 -0300

    Mensagens para Pessoa

--- src/main/java/com/starking/vendas/utils/MessagesUtils.java
+++ src/main/java/com/starking/vendas/utils/MessagesUtils.java
@@ -9,4 +9,9 @@ public class MessagesUtils {
     private static final String LANCAMENTO_NAO_ENCONTRADO = "Lançamento não encontrado";
     private static final String LANCAMENTO_JA_EXISTE = "Lançamento já existe";
     private static final String ERRO_AO_SALVAR_LANCAMENTO = "Erro ao salvar o lançamento";
+
+    // Mensagens para Pessoa
+    private static final String PESSOA_NAO_ENCONTRADA = "Pessoa não encontrada";
+    private static final String PESSOA_JA_CADASTRADA = "Pessoa já cadastrada";
+    private static final String ERRO_AO_ATUALIZAR_PESSOA = "Erro ao atualizar informações da pessoa";
 }
