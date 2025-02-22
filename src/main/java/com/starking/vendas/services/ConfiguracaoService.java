d4fe0bb389dc6548b754a20f8311a2aa0e66c80c
Pedro Rhamon <pedrorhamon16@gmail.com>
2025-02-21 21:31:19 -0300

    obterConfiguracoes e criação do service

---
+++ src/main/java/com/starking/vendas/services/ConfiguracaoService.java
@@ -0,0 +1,20 @@
+package com.starking.vendas.services;
+
+import com.starking.vendas.model.Configuracao;
+import com.starking.vendas.repositories.ConfiguracaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ConfiguracaoService {
+
+    @Autowired
+    private ConfiguracaoRepository configuracaoRepository;
+
+    public Configuracao obterConfiguracoes() {
+        List<Configuracao> lista = configuracaoRepository.findAll();
+        return lista.isEmpty() ? null : lista.get(0);
+    }
+}
