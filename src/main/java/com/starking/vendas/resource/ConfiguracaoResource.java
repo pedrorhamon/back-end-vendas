bed1671c618497abbf9cdd5e704fd539a57bc20b
Pedro Rhamon <pedrorhamon16@gmail.com>
2025-03-04 19:55:22 -0300

    Ajustando services e regras de negócio adaptado para o sistema.

--- src/main/java/com/starking/vendas/resource/ConfiguracaoResource.java
+++ src/main/java/com/starking/vendas/resource/ConfiguracaoResource.java
@@ -1,8 +1,15 @@
 package com.starking.vendas.resource;
 
 import com.starking.vendas.model.Configuracao;
+import com.starking.vendas.model.response.CategoriaResponse;
+import com.starking.vendas.model.response.ConfiguracaoResponse;
 import com.starking.vendas.services.ConfiguracaoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,12 +21,14 @@ public class ConfiguracaoResource {
     private ConfiguracaoService configuracaoService;
 
     @GetMapping
-    public Configuracao obterConfiguracoes() {
-        return configuracaoService.obterConfiguracoes();
+    public ResponseEntity<Page<ConfiguracaoResponse>> obterConfiguracoes(
+            @PageableDefault(size = 10) Pageable pageable) {
+        Page<ConfiguracaoResponse> configuracao = this.configuracaoService.obterConfiguracoes(pageable);
+        return !configuracao.isEmpty() ? ResponseEntity.ok(configuracao) : ResponseEntity.status(HttpStatus.NOT_FOUND).body(Page.empty());
     }
 
     @PostMapping
-    public Configuracao adicionarConfiguracao(@RequestBody Configuracao configuracao) {
+    public ConfiguracaoResponse adicionarConfiguracao(@RequestBody Configuracao configuracao) {
         return configuracaoService.salvarConfiguracao(configuracao);
     }
 }
--- src/main/java/com/starking/vendas/services/ConfiguracaoService.java
+++ src/main/java/com/starking/vendas/services/ConfiguracaoService.java
@@ -1,8 +1,13 @@
 package com.starking.vendas.services;
 
+import com.starking.vendas.model.Categoria;
 import com.starking.vendas.model.Configuracao;
+import com.starking.vendas.model.response.CategoriaResponse;
+import com.starking.vendas.model.response.ConfiguracaoResponse;
 import com.starking.vendas.repositories.ConfiguracaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,12 +18,14 @@ public class ConfiguracaoService {
     @Autowired
     private ConfiguracaoRepository configuracaoRepository;
 
-    public Configuracao obterConfiguracoes() {
-        List<Configuracao> lista = configuracaoRepository.findAll();
-        return lista.isEmpty() ? null : lista.get(0);
+    public Page<ConfiguracaoResponse> obterConfiguracoes(Pageable pageable) {
+        Page<Configuracao> configuracaoPage = configuracaoRepository.findAll(pageable);
+        return configuracaoPage.map(ConfiguracaoResponse::new);
     }
 
-    public Configuracao salvarConfiguracao(Configuracao configuracao) {
-        return configuracaoRepository.save(configuracao);
+    public ConfiguracaoResponse salvarConfiguracao(Configuracao configuracao) {
+        Configuracao configuracaoSalva = configuracaoRepository.save(configuracao);
+
+        return new ConfiguracaoResponse(configuracaoSalva);
     }
 }
